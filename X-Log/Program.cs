95fcc8b7ad53a7bb05ece772389aa68609f37fe3
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-10-30 14:14:30 +0300

    Initial commit

---
+++ X-Log/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace X_Log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
