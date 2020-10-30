f0317ce89851aa9382c15f365c09a3d6b10a81cf
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-10-30 16:48:28 +0300

    Created Logger

--- X-Log/Program.cs
+++ X-Log/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using System.Threading;
+using X_Log.XLog;
 
 namespace X_Log
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logBuffer = new LogBuffer(20, 2000);
+            logBuffer.Add("1");
+            logBuffer.Add("2");
+            Thread.Sleep(10000);
+            logBuffer.Add("3");
+            logBuffer.Flush();
         }
     }
 }
---
+++ X-Log/XLog/LogBuffer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace X_Log.XLog
+{
+    public class LogBuffer
+    {
+        public readonly int AutoLogDelay;
+        public readonly int MaxQueueLength;
+        public readonly string LogFileName;
+        
+        private readonly Timer _autoFlushTimer;
+        private readonly object _queueLock = new object();
+        private readonly Queue<string> _queue = new Queue<string>();
+        private int _flushing;
+
+        public LogBuffer(int maxQueueLength, int autoLogDelay)
+        {
+            var now = DateTime.Now;
+
+            MaxQueueLength = maxQueueLength;
+            AutoLogDelay = autoLogDelay;
+            LogFileName = "xlog-" + now.Day + "-" + now.Month + "-" + now.Year + ".txt";
+            _autoFlushTimer = new Timer(obj => Flush(), null, AutoLogDelay, AutoLogDelay);
+        }
+
+        public async void Flush()
+        {
+            if (Interlocked.CompareExchange(ref _flushing, 1, 0) == 0)
+            {
+                var lines = new List<string>();
+                lock (_queueLock)
+                {
+                    lines.AddRange(_queue);
+                    _queue.Clear();
+                }
+
+                Console.WriteLine("Logging " + lines.Count + " messages");
+                try
+                {
+                    await File.AppendAllLinesAsync(LogFileName, lines);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+                finally
+                {
+                    _flushing = 0;
+                }
+            }
+        }
+
+        public void Add(string s)
+        {
+            int queueLength;
+            lock (_queueLock)
+            {
+                _queue.Enqueue(CreateLogString(s));
+                queueLength = _queue.Count;
+            }
+
+            if (queueLength >= MaxQueueLength) Flush();
+        }
+
+        private string CreateLogString(string s)
+        {
+            var now = DateTime.Now;
+            var hour = now.Hour.ToString().PadLeft(2, '0');
+            var minute = now.Minute.ToString().PadLeft(2, '0');
+            var second = now.Second.ToString().PadLeft(2, '0');
+            return "XLog[" + hour + ":" + minute + ":" + second + "]: " + s;
+        }
+    }
+}
